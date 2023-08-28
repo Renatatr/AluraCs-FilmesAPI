e86b1fb339d6416a06706139b2f9fad254b515bd
Renatatr <78363362+Renatatr@users.noreply.github.com>
2023-08-28 15:14:51 -0300

    Utilizando Dtos

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O Título do filme é obrigatório")]
+    public string Titulo { get; set; }
+    
+    [Required(ErrorMessage = "O Genero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O tamanho não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    
+    [Required(ErrorMessage = "A Duração do filme é obrigatório")]
+    [Range(70,600, ErrorMessage = "Deve ter entre 70 e 600min")]
+    public int Duracao { get; set; }
+}
